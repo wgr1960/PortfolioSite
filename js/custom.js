5fd07de2c319b7410c15c027555ead0762c2da46
Willie Rodriguez <wgr1960@gmail.com>
2015-11-03 22:55:59 -0800

    - Added more content to index.html - Added more framework CSS and style CSS - Added js function - deleted Roboto font, accessed through CDN - Added new images

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,39 @@
+$(window).resize(function() {
+    if ($(this).width() > 580) {
+          $(window).scroll( function(){
+            $('.experience-block').each(function(i){            
+                var objectBottom = $(this).offset().top + $(this).outerHeight();
+                var windowBottom = $(window).scrollTop() + $(window).height();            
+                if( objectBottom < windowBottom ){                
+                    $(this).animate({'opacity':'1'}, 150);                    
+                }          
+            });   
+        }); 
+    }
+    else {
+        $('.experience-block').each(function(i){
+            $(this).css( "opacity", "1" );
+        });            
+    }
+});
+
 $(document).ready(function() {
+    //Function that displays hamburger menu contents
+    var hamMenu = document.getElementsByClassName('.hamburger-menu#hamburger');
+
+    hamMenu.onclick = function(hamMenu){
+        var menu = document.getElementsByClassName('.dropdown');
+
+        if(menu.style.display == 'block'){
+            menu.style.display = 'none';
+        }
+        else {
+            menu.style.display = 'block';                    
+        }
+    };
+
     // Function to make experience blocks fade in
-    $(window).scroll( function(){
-        $('.experience-block').each(function(i){            
-            var objectBottom = $(this).offset().top + $(this).outerHeight();
-            var windowBottom = $(window).scrollTop() + $(window).height();            
-            if( objectBottom < windowBottom ){                
-                $(this).animate({'opacity':'1'},150);                    
-            }          
-        });   
-    }); 
+    $(window).resize();
 
     //Create a clone of my menu
     $('.navbar').addClass('original').clone().insertAfter('.navbar').addClass('scrollMenu').css('position','fixed').css('top','0').removeClass('original').hide();
@@ -20,7 +45,7 @@ $(document).ready(function() {
       var originalMenuPosition = originalMenu.offset();
       var originalMenuTop = originalMenuPosition.top;               
       checkMenuScroll(originalMenu, originalMenuPosition, originalMenuTop);
-    }
+    };
 
     //Function that checks the window position after user scrolled and attaches the menu to the top
     function checkMenuScroll(originalMenu, originalMenuPosition, originalMenuTop) {
@@ -33,7 +58,7 @@ $(document).ready(function() {
             $('.scrollMenu').hide();
             $('.original').css('visibility','visible');
           }
-    }
+    };
 
     // Changes the navbar displayed when user scrolled
     function switchMenusDisplayed(originalMenu) {
@@ -44,5 +69,5 @@ $(document).ready(function() {
         //Show scrollMenu menu, hide original menu
         $('.scrollMenu').css('left',leftMenuPosition+'px').css('top',0).css('width',originalMenuWidth).css('box-shadow', '0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)').show();
         $('.original').css('visibility','hidden');
-    }
+    };
 });
